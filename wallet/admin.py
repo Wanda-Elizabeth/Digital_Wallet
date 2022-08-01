f502d3bf3ca39955593c6a31b2a4f27793a7aaa0
Wanda-Elizabeth <wandaelizabeth@gmail.com>
2022-08-01 22:34:49 +0300

    Wallet

---
+++ wallet/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Account, Card, Customer, Loan, Notifications, Receipt, Reward, Third_Party, Transaction, Wallet
+
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=("first_name","last_name","email")
+    search_fields=("fast_name","lastname",)
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Wallet)
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Card)
+admin.site.register(Third_Party)
+admin.site.register(Notifications)
+admin.site.register(Receipt)
+admin.site.register(Loan)
+admin.site.register(Reward)
+
+
+
+
+    
+    
+
